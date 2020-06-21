9e0c5dd55232f06126c889d33c176a10a05542cf
laosanli1 <14773997531@qq.com>
2020-06-21 09:43:25 +0800

    first idea&git

---
+++ TestGit1/src/main/java/com/lcz/TestGit1.java
@@ -0,0 +1,15 @@
+package com.lcz;
+
+/**
+ * ClassName:TestGit1
+ * Package:com.lcz
+ * Description:
+ *
+ * @Date:2020/6/21 9:35
+ * @Author:$licunzhuang@13253633031
+ */
+public class TestGit1 {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
