a9d7efe59a392f0f307cbdd74f47dd698dbf39e4
laosanli1 <14773997531@qq.com>
2020-06-21 09:46:04 +0800

    更新2

--- TestGit1/src/main/java/com/lcz/TestGit1.java
+++ TestGit1/src/main/java/com/lcz/TestGit1.java
@@ -12,5 +12,6 @@ public class TestGit1 {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("gengxin");
+        System.out.println("更新2");
     }
 }
