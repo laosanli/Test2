8149f4eb2d1bc74e5da2d5b6e3c32f883cd61a90
laosanli1 <14773997531@qq.com>
2020-06-21 10:18:33 +0800

    分支添加2

--- TestGit1/src/main/java/com/lcz/TestGit1.java
+++ TestGit1/src/main/java/com/lcz/TestGit1.java
@@ -17,5 +17,6 @@ public class TestGit1 {
         System.out.println("创建分支1");
         System.out.println("创建分支2");
         System.out.println("分支添加");
+        System.out.println("分支添加2");
     }
 }
